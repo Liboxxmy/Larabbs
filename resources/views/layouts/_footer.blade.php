9b26e8aec0d3be595696761288b5cf7be14993da
BOBO <497132472@qq.com>
2019-05-15 11:13:15 +0800

    finish basci page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/u/2660556331?is_all=1" target="_blank">Li</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:497132472@qq.com">联系我们</a></p>
+    </div>
+</footer>
