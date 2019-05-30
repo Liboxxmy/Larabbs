c15fcd6877eb9271fdf226db1cd64374f9b0141c
BOBO <497132472@qq.com>
2019-05-30 11:25:20 +0800

    auto translate slug

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -28,7 +28,7 @@
                         </a>
 
                         <span> • </span>
-                        <a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
+                        <a href="{{ $topic->link() }}" title="{{ $topic->user->name }}">
                             <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                             {{ $topic->user->name }}
                         </a>
